6ad4270f6a52a869a3bd16911bb5afeecd5d6d6f
MrBenGriffin <ben@redsnapper.net>
2010-09-08 13:36:49 +0000

    BUG: key encoder was broken.

--- core/osimessage.cpp
+++ core/osimessage.cpp
@@ -568,14 +568,11 @@ void OsiMessage::construct_header_value(header& h) {
 			}
 			h.x = hx.str();
 		} break;
+		case rescookie: { //special cookie parse.
+			h.n = "Set-Cookie";
+		} // no break - using reqcookie code also.
 		case reqcookie: { //special cookie parse.
 			h.x.append(" name=\"" + h.n + "\"");
-			do_comments(h.v,h.comments);  //will leave the value (if there is one) followed by a ;
-			h.s = h.v; h.v.clear();
-			do_header_subheads(h);
-		} break;
-		case rescookie: { //special cookie parse.
-			h.x.append(" name=\"Set-Cookie");
 			do_comments(h.v,h.comments);  //will leave the value (if there is one) followed by a ;
 			h.s = h.v; h.v.clear();
 			do_header_subheads(h);
