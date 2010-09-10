530171b9024fb7c4e0076bce7c245d90df0aa161
MrBenGriffin <ben@redsnapper.net>
2010-09-10 13:05:48 +0000

    Fixed querstring urls - but only on good xml.

--- core/main.cpp
+++ core/main.cpp
@@ -52,7 +52,7 @@ void finalise();
 void shutdown();
 
 int main(int argc, char *argv[]) {
- 	string v_number = "1.100909"; //Do NOT put the v here!
+ 	string v_number = "1.100910"; //Do NOT put the v here!
 #ifdef FAST
 	string version  = "Obyx v"+v_number+"F Supported";
 #else
