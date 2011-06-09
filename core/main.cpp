e27e97254c856786767fed53a5125d79408cea57
MrBenGriffin <ben@redsnapper.net>
2011-06-09 10:16:50 +0000

    v1.110609 	BUG.		(arithmetic,bitwise) unary minus being misidentified when - appears after brackets, functions.

--- core/main.cpp
+++ core/main.cpp
@@ -55,7 +55,7 @@ void finalise();
 void shutdown();
 
 int main(int argc, char *argv[]) {
-	string v_number = "1.110524"; //Do NOT put the v here!
+	string v_number = "1.110609"; //Do NOT put the v here!
 #ifdef FAST
 	string version  = "Obyx v"+v_number+"F Supported";
 #else
