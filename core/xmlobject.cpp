00eac9f5877ddbfc7b76fa3e7bd212825b3374dd
MrBenGriffin <ben@redsnapper.net>
2011-06-17 19:01:38 +0000

    BUG.		when writing to an xdoc, the entire document may be deleted.

--- core/xmlobject.cpp
+++ core/xmlobject.cpp
@@ -428,6 +428,10 @@ bool XMLObject::xp(const DataItem* ins,const u_str& path,DOMLSParser::ActionType
 		}
 		xpr->release();
 		xpr = NULL;
+		x_doc->normalizeDocument(); //necessary for things like document deletion.
+		if (x_doc->getDocumentElement() == NULL) {
+			x_doc->release(); x_doc = NULL;
+		}
 	} else {
 		if (node_expected) {
 			if (error_str.empty()) {
