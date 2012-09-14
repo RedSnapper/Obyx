79d47e822530a7be04f144f4f44d997ca4f6f5ea
MrBenGriffin <ben@redsnapper.net>
2012-09-14 15:36:25 +0000

    very odd appearance of apparent mismatch.

--- core/main.cpp
+++ core/main.cpp
@@ -56,7 +56,7 @@ void finalise();
 void shutdown();
 
 int main(int argc, char *argv[]) {
-	string v_number = "1.120914"; //Do NOT put the v here!
+	string v_number = "1.120915"; //Do NOT put the v here!
 #ifdef FAST
 #ifdef PROFILING
 	int  profilecount = 1;
