c916029dc3d48d0bbc1f6a5a928089d359f21d87
Ben Griffin <ben@redsnapper.net>
2015-03-26 17:59:06 +0000

    rewritten to use environment variable directly.

--- commons/vdb/connection.cpp
+++ commons/vdb/connection.cpp
@@ -37,18 +37,26 @@ namespace Vdb {
 	
 	Connection::~Connection() { 
 	}
+
+	//Environment* e =Environment::service(); string site="";
+	//if( && (site.compare("config")==0)) {
+	//	*Logger::log << Log::fatal << Log::LI << "MySQLConnection:: Opening via configfile:" << file << Log::LO << Log::blockend;
+	//	}
+	
+	
+	
 	
 	//syntactic sugar.
 	bool Connection::connect() {
-		bool retval=false;
+		bool retval=false; string filename="";
 		Environment* e = Environment::service();
-		if(e->SQLconfig_file().empty()) {
+		if(! e->getenv("OBYX_SQLCONFIG_FILE",filename) || filename.empty()) {
 			retval = open( e->SQLhost(),e->SQLuser(),e->SQLport(),e->SQLuserPW());
 			if (retval) {
 				retval = database(Environment::Database());
 			}
 		} else {
-			retval = open( e->SQLconfig_file());
+			retval = open( filename );
 		}
 		return retval;
 	}
