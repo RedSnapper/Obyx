6d31e20f698b4dbfa97bdbba095903f161867801
Ben Griffin <ben@redsnapper.net>
2015-03-26 17:51:16 +0000

    really poor work.

--- commons/vdb/mysql/mysqlconnection.cpp
+++ commons/vdb/mysql/mysqlconnection.cpp
@@ -52,6 +52,11 @@ namespace Vdb {
 	}
 	
 	bool MySQLConnection::open(const std::string& file) { //use a config file
+		 Environment* e =Environment::service(); string site="";
+		 if(e->getenv("SITE_NAME",site) && (site.compare("config")==0)) {
+			 *Logger::log << Log::fatal << Log::LI << "MySQLConnection:: Opening via configfile:" << file << Log::LO << Log::blockend;
+		 }
+		
 		if ( ! isopen() ) {
 			if (s->options(connectionHandle,MYSQL_READ_DEFAULT_FILE,file.c_str()) == 0) {
 				thost = file;
@@ -61,11 +66,6 @@ namespace Vdb {
 					conn_open = false;
 					db_open = false;
 				} else {
-					Environment* e =Environment::service();
-					string site="";
-					if(e->getenv("SITE_NAME",site) && site.compare("config")==0) {
-						*Logger::log << Log::fatal << Log::LI << "MySQLConnection error:: Connection apparently succeeded." << Log::LO << Log::blockend;
-					}
 					conn_open = true;
 					db_open = true;
 				}
@@ -81,6 +81,11 @@ namespace Vdb {
 	}
 
 	bool MySQLConnection::open(const std::string& host, const std::string& user, const unsigned int port,const std::string& password) {
+		Environment* e =Environment::service(); string site="";
+		if(e->getenv("SITE_NAME",site) && (site.compare("config")==0)) {
+			*Logger::log << Log::fatal << Log::LI << "MySQLConnection:: Opening via host/port/etc:" << host << Log::LO << Log::blockend;
+		}
+
 		if ( ! isopen() ) {
 			const char* tsocket = nullptr;
 			thost = host;
