12a0f78844ec8e1e2b4c5b474cd2c0b66c4eda2d
Ben Griffin <ben@redsnapper.net>
2016-06-08 12:37:31 +0100

    version.

--- core/main.cpp
+++ core/main.cpp
@@ -56,7 +56,7 @@ void finalise();
 void shutdown();
 
 int main(int argc, char *argv[]) {
-	string v_number = "1.150909"; //Do NOT put the v here!
+	string v_number = "1.160608"; //Do NOT put the v here!
 #ifdef FAST
 #ifdef PROFILING
 	int  profilecount = 40;
