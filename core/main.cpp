89c1502d74b1bd8c17017b8104a3461a953691b0
Mr Ben Griffin <ben@redsnapper.net>
2023-07-06 14:46:16 +0100

    Update main.cpp
    version date.

--- core/main.cpp
+++ core/main.cpp
@@ -56,7 +56,7 @@ void finalise();
 void shutdown();
 
 int main(int argc, char *argv[]) {
-	string v_number = "1.180131"; //Do NOT put the v here!
+	string v_number = "1.230706"; //Do NOT put the v here!
 #ifdef FAST
 #ifdef PROFILING
 	int  profilecount = 40;
